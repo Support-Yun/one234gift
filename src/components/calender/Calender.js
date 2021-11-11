3ca9287043f2d1e68269bea53c332c178f73754a
ArtSwing <hbyoo94@naver.com>
2021-11-11 16:00:07 +0900

    커스터머 페이지 그리드70% 작업완료

---
+++ src/components/calender/Calender.js
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import Stack from '@mui/material/Stack';
+import TextField from '@mui/material/TextField';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import TimePicker from '@mui/lab/TimePicker';
+import DateTimePicker from '@mui/lab/DateTimePicker';
+import DesktopDatePicker from '@mui/lab/DesktopDatePicker';
+import MobileDatePicker from '@mui/lab/MobileDatePicker';
+
+export default function MaterialUIPickers() {
+  const [value, setValue] = React.useState(new Date('2021-11-11T21:11:54'));
+
+  const handleChange = (newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <Stack spacing={3}>
+        <DateTimePicker
+          label="날짜/시간선택"
+          value={value}
+          onChange={handleChange}
+          renderInput={(params) => <TextField {...params} />}
+        />
+      </Stack>
+    </LocalizationProvider>
+  );
+}
--- src/pages/Customer.js
+++ src/pages/Customer.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import {
     Paper,
     TextField,
@@ -6,23 +6,55 @@ import {
     Grid,
     MenuItem,
     Button,
-    Container
+    Container,
 } from '@mui/material';
 import Page from '../components/Page';
+import Calender from '../components/calender/Calender';
+import RadioGroupRating from '../components/Rating/Rating';
 import DataTable from '../components/table/Table';
-import DenseTable from '../components/table/DenseTable';
+import UnstyledSwitches from '../components/Switch/Switch';
+import MaxHeightTextarea from '../components/Textfeild/Textfeild';
 
 export default function Customer() {
     const [customerName, setCustomerName] = useState("");
     const [category, setCategory] = useState("분류");
     const [location, setLocation] = useState("지역");
-    const [customerInfomation, setcustomerinfomation] = useState("");
+    const [customerInfomation, setcustomerinfomation] = useState(
+        {
+            customerId: 1,
+            name: "성동우체국",
+            category: "우체국",
+            location: "서울 특별시",
+            state: "활성화",
+            officeid: "123312312",
+            fax: "032-85-5652",
+            managername: "박선규",
+            managerId: 1,
+            managerRank: "사원",
+            email: "qwe@naverr.com",
+            managerPhone: "010-2622-2665",
+            otherPhone: "010-5522-2251"
+        }
+    );
 
+    const [CustomWriterInfo, setWriterInfo] = useState({
+        WriterId: 1,
+        name: "김말숙",
+        managerPhone: "010-2622-2665",
+        managerRank: '사원'
+    })
+    const [noteWrite, setnoteWrite] = useState({
+        WriterId: 1,
+        name: "김말숙",
+        managerPhone: "010-2622-2665",
+        managerRank: '사원',
+        note: '동해물과백두산이 마르고 닳도록'
+    })
 
     function customerinfomationHandler(target) {
         setcustomerinfomation(target.value);
-
-
+    }
+    function CustiomerInfomap() {
     }
     /**
      * 고객명 변경 핸들러
@@ -60,7 +92,6 @@ export default function Customer() {
         setCustomerName("");
         setCategory('분류');
         setLocation('지역');
-        setcustomerinfomation("");
     }
 
     /**
@@ -96,51 +127,21 @@ export default function Customer() {
     /*
      클릭된 테이블 행 값 가져오는 함수
     */
-    
 
-    function getMockCustomers() {
-        return [
-            {
-                customerId : 1,
-                name : "고객 업체명",
-                category : "우체국",
-                location : "서울 특별시"                
-            },
-            {
-                customerId : 2,
-                name : "고객 업체명",
-                category : "우체국",
-                location : "서울 특별시"                
-            },
-            {
-                customerId : 3,
-                name : "고객 업체명",
-                category : "우체국",
-                location : "서울 특별시"                
-            },
-            {
-                customerId : 4,
-                name : "고객 업체명",
-                category : "우체국",
-                location : "서울 특별시"
-            },
-            {
-                customerId : 5,
-                name : "고객 업체명",
-                category : "우체국",
-                location : "서울 특별시"
-            },
-        ]
-    }
 
-    const customerTableHeaderColums = ["이름", "분류", "지역"];
+
+
+    const customerTableHeaderColums = ["업체명", "사업자번호", "상태", "분류", "FAX"];
+    const customerinfoTableHeader = ["담당자명", "직급", "이메일", "메인연락처", "기타 연락처"];
+    const Writerinfo = ["작성자", "직위", "연락처"];
+    const NoteWriterinfo = ["#","담당자", "직위", "연락처","메모내역"]
 
     const categoryList = ["분류", "은행", "우체국"];
     const locationList = ["지역", "서울특별시", "부산광역시"];
     const margin10Right = { marginRight: 10, width: '25%' };
     return (
         <Page title="User | Minimal-UI">
-            
+
             <Container maxWidth="xl">
                 <Grid container spacing={3}>
                     <Grid item md={12} sm={6} xs={6}>
@@ -175,7 +176,7 @@ export default function Customer() {
                                     header={["고객명", "상태", "분류", "지역"]}
                                     data={[
                                         { when: '서울', type: 'ksjdfh', name: '김기동', onClick: () => customerinfomationHandler },
-                                        { when: '제주', type: 'ksjdfh', name: '박기동', onClick: () => alert() },
+                                        { when: '제주', type: 'ksjdfh', name: '박기동', onClick: () => { } },
                                         { when: '충남', type: 'ksjdfh', name: '이기동', onClick: () => alert() },
                                         { when: '전남', type: 'ksjdfh', name: '정기동', onClick: () => alert() },
                                         { when: '강원', type: 'ksjdfh', name: '안기동', onClick: () => alert() },
@@ -186,35 +187,94 @@ export default function Customer() {
                         </Paper>
                     </Grid>
 
-                    <Grid item xs={12} md={8} lg={8}>
+                    <Grid item xs={12} md={6} lg={8}>
                         <Paper elevation={3} style={{ padding: 20, margin: 20 }}>
-                            <table>
+                            <table style={{ width: "100%", textAlign: "center" }}>
                                 <thead>
                                     {
-                                        customerTableHeaderColums.map((colums)=>(
+                                        customerTableHeaderColums.map((colums) => (
                                             <th>{colums}</th>
                                         ))
                                     }
                                 </thead>
                                 <tbody>
-                                    {
-                                        getMockCustomers().map((customer)=>(
-                                            <tr onClick={()=>{
-                                                alert(customer.customerId);
-                                            }}>
-                                                <td>{customer.name}</td>
-                                                <td>{customer.category}</td>
-                                                <td>{customer.location}</td>
-                                            </tr>
-                                        ))
-                                    }
+                                    <td>{customerInfomation.name}</td>
+                                    <td>{customerInfomation.officeid}</td>
+                                    <td>{customerInfomation.state}</td>
+                                    <td>{customerInfomation.category}</td>
+                                    <td>{customerInfomation.fax}</td>
                                 </tbody>
                             </table>
-                        </Paper> 
+                        </Paper>
                         <Paper elevation={3} style={{ padding: 20, margin: 20 }}>
-                            DFKJSHFJKDS
+                            <table style={{ width: "100%", textAlign: "center" }}>
+                                <thead >
+                                    {
+                                        customerinfoTableHeader.map((colums) => (
+                                            <th>{colums}</th>
+                                        ))
+                                    }
+                                </thead>
+                                <tbody style={{ textAlign: "center" }}>
+                                    <td>{customerInfomation.managername}</td>
+                                    <td>{customerInfomation.managerRank}</td>
+                                    <td>{customerInfomation.email}</td>
+                                    <td>{customerInfomation.managerPhone}</td>
+                                    <td>{customerInfomation.otherPhone}</td>
+                                </tbody>
+                            </table>
                         </Paper>
                     </Grid>
+                    <Grid item xs={12} md={6} lg={6}>
+                        <Paper elevation={3} style={{ padding: 20, margin: 20 }}>
+                            <table style={{ width: "100%", textAlign: "center" }}>
+                                <thead >
+                                    {
+                                        Writerinfo.map((colums) => (
+                                            <th>{colums}</th>
+                                        ))
+                                    }
+                                </thead>
+                                <tbody style={{ textAlign: "center" }}>
+                                    <td>{CustomWriterInfo.name}</td>
+                                    <td>{CustomWriterInfo.managerRank}</td>
+                                    <td>{CustomWriterInfo.managerPhone}</td>
+                                </tbody>
+                            </table>
+                            <br />
+                            <div style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
+                                <Calender />
+                                <div style={{ margin: "0px", width: "150px", display: "flex", alignItems: "center", justifyContent: "center" }}>
+                                    샘플여부<UnstyledSwitches />
+                                </div>
+                                <div style={{ margin: "30px", display: "flex" }}>
+                                    반응    <RadioGroupRating />
+                                </div>
+                            </div>
+                        </Paper>
+                        <Paper elevation={3} style={{ padding: 20, margin: 20 }}>
+                            <MaxHeightTextarea />
+                        </Paper>
+                        <Paper elevation={3} style={{ padding: 20, margin: 20 }}>
+                            <table style={{ width: "100%", textAlign: "center" }}>
+                                <thead>
+                                    {
+                                        NoteWriterinfo.map((colums) => (
+                                            <th>{colums}</th>
+                                        ))
+                                    }
+                                </thead>
+                                <tbody>
+                                    <td>{noteWrite.WriterId}</td>
+                                    <td>{noteWrite.name}</td>
+                                    <td>{noteWrite.managerRank}</td>
+                                    <td>{noteWrite.managerPhone }</td>
+                                    <td>{noteWrite.note}</td>
+                                </tbody>
+                            </table>
+                        </Paper>
+                    </Grid>
+
                 </Grid>
             </Container>
 
