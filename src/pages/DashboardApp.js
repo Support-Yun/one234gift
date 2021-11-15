2cb89b1c043c28dab0db1b6ecf4e4c560c10c3e0
wodyd202 <wodyd202@daum.net>
2021-11-15 16:15:46 +0900

    대시보드 예약콜 리스트 구현

--- src/pages/DashboardApp.js
+++ src/pages/DashboardApp.js
@@ -26,51 +26,16 @@ export default function DashboardApp() {
           <Grid item xs={12} sm={12} md={12}>
             <AppQuickMenu />
           </Grid>
-
-          <Grid item xs={12} md={6} lg={4}>
-            <AppWebsiteVisits />
-          </Grid>
-
-          <Grid item xs={12} md={6} lg={8}>
-            {/* <AppCurrentVisits /> */}
-            {/* <AppNotice /> */}
+          <Grid item xs={12} md={12} lg={12}>
             <Grid container spacing={2}>
-              <Grid item xs = {12} md={12} lg={12}>
-                <AppNotice />
-              </Grid>
               <Grid item xs = {12} md={12} lg={12}>
                 <AppCumulativeSales />
               </Grid>
+              <Grid item xs = {12} md={12} lg={12}>
+                <AppNotice />
+              </Grid>
             </Grid>
           </Grid>
-
-          <Grid item xs={12} md={12} lg={12}>
-            {/* <AppCumulativeSales /> */}
-          </Grid>
-
-          {/* <Grid item xs={12} md={6} lg={8}>
-            <AppConversionRates />
-          </Grid>
-
-          <Grid item xs={12} md={6} lg={4}>
-            <AppCurrentSubject />
-          </Grid>
-
-          <Grid item xs={12} md={6} lg={8}>
-            <AppNewsUpdate />
-          </Grid>
-
-          <Grid item xs={12} md={6} lg={4}>
-            <AppOrderTimeline />
-          </Grid>
-
-          <Grid item xs={12} md={6} lg={4}>
-            <AppTrafficBySite />
-          </Grid>
-
-          <Grid item xs={12} md={6} lg={8}>
-            <AppTasks />
-          </Grid> */}
         </Grid>
       </Container>
     </Page>
